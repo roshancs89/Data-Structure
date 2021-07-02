060a3b48e255cdde5141b09142c43280dbdf42c1
Roshan <roshancs10@gmail.com>
2021-07-02 10:28:01 +0530

    Linkedlist problems

---
+++ src/main/java/org/ds/linkedlist/DeleteNode.java
@@ -0,0 +1,81 @@
+package org.ds.linkedlist;
+
+
+
+public class DeleteNode {
+
+    Node head;
+    static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    /**
+     * This program inserts node at the beginning of list
+     * @param new_data - new Node value
+     */
+    public void push(int new_data){
+        Node newNode = new Node(new_data);
+
+        newNode.next = this.head;
+
+        this.head = newNode;
+    }
+
+    /**
+     * Prints the elements of LinkedList
+     */
+    public void printList(){
+
+        Node prevNode = this.head;
+
+        while(prevNode != null){
+            System.out.print(prevNode.data + " ");
+            prevNode = prevNode.next;
+        }
+
+        System.out.println(" ");
+    }
+
+    public void deleteNode(int position){
+        Node temp = this.head;
+
+        if(position == 0){
+            this.head = temp.next;
+            return;
+        }
+
+        for(int i =0; temp != null && i < position-1; i++){
+            temp = temp.next;
+        }
+
+        if(temp == null || temp.next == null)
+            return;
+
+        temp.next = temp.next.next;
+    }
+
+    public static void main(String args[]){
+        System.out.println("Program to delete the node of linkedlist");
+        DeleteNode linkedList = new DeleteNode();
+        linkedList.push(1);
+        linkedList.push(2);
+        linkedList.push(3);
+        linkedList.printList();
+        linkedList.deleteNode(0);
+        linkedList.printList();
+        linkedList.push(3);
+        linkedList.printList();
+        linkedList.deleteNode(2);
+        linkedList.printList();
+        linkedList.push(2);
+        linkedList.printList();
+        linkedList.deleteNode(1);
+        linkedList.printList();
+    }
+}
